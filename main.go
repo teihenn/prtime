68ef958c4e7b038fa6f349e62edaddf0e2554bf5
yo1956 <hm.hr.yossy@gmail.com>
2024-06-20 00:04:43 +0900

    fix: configFilePath in main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	configFilePath := "/Users/y_yoshida/Projects/rrader/internal/config/config.yml"
+	configFilePath := "/Users/y_yoshida/Projects/prtime/internal/config/config.yml"
 	config, err := config.Load(configFilePath)
 	if err != nil {
 		fmt.Printf("Error loading config: %s\n", err)
